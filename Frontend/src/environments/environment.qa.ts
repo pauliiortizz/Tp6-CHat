053af9938a79ae0259a9f14345f3a5de5b8b8726
pauliiortizz <127853900+pauliiortizz@users.noreply.github.com>
2025-10-22 00:06:56 -0300

    Add QA environment configuration

---
+++ Frontend/src/environments/environment.qa.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  qa: true,
+  // Actualizá esta URL al dominio público de tu API en Azure/Clever Cloud
+  apiUrl: 'https://productosweb-fpcnfsbsg8gscbat.brazilsouth-01.azurewebsites.net/api/Product'  // URL de la API para prod (productos)
+};
