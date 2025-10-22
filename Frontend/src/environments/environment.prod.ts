60ab66efd18dcea2e7019bbc43a558ec61dfce62
pauliiortizz <127853900+pauliiortizz@users.noreply.github.com>
2025-10-22 00:05:45 -0300

    Update production API URL in environment file

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Actualizá esta URL al dominio público de tu API en Azure/Clever Cloud
-  apiUrl: 'https://tu-api.azurewebsites.net/api/Product'  // URL de la API para prod (productos)
+  apiUrl: 'https://productosweb-prod-gbfnaef6a3c7e3e7.brazilsouth-01.azurewebsites.net//api/Product'  // URL de la API para prod (productos)
 };
